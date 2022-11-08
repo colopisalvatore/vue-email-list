2214f8e2608f60aca7f73f09e38b89772f3f8986
colopisalvatore <salvatorecolopi1989@gmail.com>
2022-11-08 15:12:32 +0100

    completato senza stile

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,23 @@ const { createApp } = Vue;
 
 createApp({
     data() {
-        return{
+        return {
+            rndEmails: []
+        }
+    },
+    created() {
+        this.genMail();
+    },
+    methods: {
+        test() {
+            this.console.log(message);
+        },
+        genMail() {
+            for (let i = 0; i < 10; i++) {
+                axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`).then((serverMsg)=>{
+                        this.rndEmails.push(serverMsg.data.response);
+                })
+            }
+        }
     }
-  }
-}).mount('#app')
+}).mount("#app");
